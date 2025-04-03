ac8112270f2e26389fd5fffb75298b47670ab8e4
thiago-cloud <thiago.154ti@gmail.com>
2025-04-03 17:19:21 -0300

    A origem do software - Criação da estrutura de diretorios do projeto - Criação da camada de conexão com o banco de dados - Criação da tela de login para authenticação utilizando java Swing

---
+++ src/br/com/servixos/dao/ModuloConexao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.servixos.dao;
+
+import com.sun.jdi.connect.spi.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Smaug
+ */
+public class ModuloConexao {
+
+    // Método responsavel por estabelecer a conexão com o banco
+    public static java.sql.Connection connector() {
+        java.sql.Connection conexao = null;
+        // Chama o driver
+        String driver = "com.mysql.jdbc.Driver";
+        // Armazena informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbservixos";
+        String user = "root";
+        String password = "970216";
+        // Estabelecendo a conexão
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        }catch (Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+
+}
